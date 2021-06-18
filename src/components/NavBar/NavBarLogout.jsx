958f52551d8de958cbc2dabd26e7814a19cbfbe8
Montserrat Cifuentes <cotamon88@gmail.com>
2021-06-18 16:30:24 +0200

    Creación y camboios en Navbarlogin y NavbarRegisterLogin

--- src/components/NavBar/NavBarLogout.jsx
+++ src/components/NavBar/NavBarLogout.jsx
@@ -10,28 +10,25 @@ import { Flex, Box, Spacer, Menu, Image,
   function NavBarLogout() {
       
     return (
-      <Flex>
-        <Box p={2}>
-           <Image  w={5} h={5} src="./images/airbnb-logo.png" />
-        </Box>
-        <Spacer />
-        <Box p={2}>
-          <Link to="/"><Button mr={2} colorScheme="blue">Home</Button></Link>
-            <Menu>
-          
-                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                    Usuario
-                </MenuButton>
-                <MenuList>
-                    <Link to="/user-register"><MenuItem>Register</MenuItem></Link>
-                    <Link to="/edit-user"><MenuItem>Editar mis datos</MenuItem></Link>
-                    <MenuItem>Editar mis alojamientis</MenuItem>
-                    <MenuItem>Logout</MenuItem>
-                </MenuList>
-            </Menu>
-        </Box>
-      </Flex>
-      );
-    }
+        <Flex paddingTop="1%" paddingLeft="1%" paddingRight="1%" paddingFoot="1%">
+            <Box p={2}>
+                <Image w={40} h={12} src="./images/airbnb-logo.png"/> 
+            </Box>
+                <Spacer />
+            <Box p={2}>
+                <Link to="/"><Button mr={2} colorScheme="blue">Hazte anfitrión</Button></Link>
+                <Menu>
+                    <MenuButton as={Button} leftIcon={<HamburgerIcon />} rightIcon = {<ChevronDownIcon />}  borderRadius="md"> </MenuButton>
+                        <MenuList>
+                            <Link to="/edit-user"><MenuItem>Editar perfil</MenuItem></Link>
+                            <Link to="/edit-user"><MenuItem>Editar alojamientos</MenuItem></Link>
+                            <Link to="/edit-user"><MenuItem>Cerrar Sesión</MenuItem></Link>
+                            <Link to="/edit-user"><MenuItem>Ayuda</MenuItem></Link>
+                        </MenuList>
+                </Menu>
+            </Box>
+        </Flex>
+        );
+      }
     
   export default NavBarLogout;
--- src/components/NavBar/NavBarRegisterLogin.jsx
+++ src/components/NavBar/NavBarRegisterLogin.jsx
@@ -5,28 +5,27 @@ import { Flex, Box, Spacer, Menu, Image,
   MenuList,
   MenuItem,
   Button, Text } from "@chakra-ui/react"
-  import { ChevronDownIcon } from '@chakra-ui/icons'
+import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons'
+
 
 function NavBarRegisterLogin() {
     
   return (
-    <Flex>
+    <Flex paddingTop="1%" paddingLeft="1%" paddingRight="1%" paddingFoot="1%">
       <Box p={2}>
-         <Image  w={8} h={8} src="./images/logo_negro-airbnb.png" />
+        <Image w={40} h={12} src="./images/airbnb-logo.png"/> 
       </Box>
-      <Spacer />
+        <Spacer />
       <Box p={2}>
-      <Link to="/"><Button mr={2} colorScheme="blue">Home</Button></Link>
+      <Link to="/"><Button mr={2} colorScheme="blue">Hazte anfitrión</Button></Link>
         <Menu>
-        
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-            Usuario
-          </MenuButton>
+          <MenuButton as={Button} leftIcon={<HamburgerIcon />} rightIcon = {<ChevronDownIcon />}  borderRadius="md"> </MenuButton>
           <MenuList>
-            <Link to="/user-register"><MenuItem>Register</MenuItem></Link>
-            <Link to="/edit-user"><MenuItem>Editar mis datos</MenuItem></Link>
-            <MenuItem>Editar mis alojamientis</MenuItem>
-            <MenuItem>Logout</MenuItem>
+              <Link to="/user-register"><MenuItem>Regístrate</MenuItem></Link>
+              <Link to="/edit-user"><MenuItem>Inicia Sesión</MenuItem></Link>
+              <Link to="/user-register"><MenuItem>Ofrece tu alojamiento</MenuItem></Link>
+              <Link to="/user-register"><MenuItem>Ofrece una experiencia</MenuItem></Link>
+              <Link to="/user-register"><MenuItem>Ayuda</MenuItem></Link>
           </MenuList>
         </Menu>
       </Box>
