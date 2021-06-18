995616efb28eaf587e8a0519e08e854b43855ff7
Montserrat Cifuentes <cotamon88@gmail.com>
2021-06-18 19:07:14 +0200

    cambio NavBar en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ChakraProvider} from "@chakra-ui/react";
 import NavBarRegisterLogin from "./components/NavBar/NavBarRegisterLogin";
-//import NavBarLogout from "./components/NavBar/NavBarLogout";
-
 import RegisterPage from './pages/register-page';
 import EditUserPage from './pages/edit-user-page';
-//import registerUserApi  from './utils/apiTest';
-//import loginUserApi  from './utils/apiTest';
+
 
 function App({ Component }) {
 
