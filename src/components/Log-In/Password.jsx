bf9547d97aa9b6d77496420aa5e9c10b16952bed
Carlos <dens.walker22@gmail.com>
2021-06-18 20:33:32 +0200

    RecuperarPassword

--- src/components/Log-In/Password.jsx
+++ src/components/Log-In/Password.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { Flex, Box, Spacer, Menu, MenuButton, MenuList, MenuItem,
-  Center,
-  FormControl,
-  Input,
-  FormLabel,
-  FormHelperText,
-  Button, Image } from "@chakra-ui/react"
+import { FormControl, Input, FormLabel, FormHelperText } from "@chakra-ui/react"
   import { ChevronDownIcon } from '@chakra-ui/icons'
 
 function PasswordLogIn ({value,onChange,validation}) {
