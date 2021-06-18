c720798653f5d6c1cf4e779fb4a9963ff8063992
Montserrat Cifuentes <cotamon88@gmail.com>
2021-06-18 19:23:23 +0200

    cambios App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,11 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ChakraProvider} from "@chakra-ui/react";
 import NavBarRegisterLogin from "./components/NavBar/NavBarRegisterLogin";
-//import NavBarLogout from "./components/NavBar/NavBarLogout";
-
 import RegisterPage from './pages/register-page';
 import EditUserPage from './pages/edit-user-page';
-<<<<<<< HEAD
-//import registerUserApi  from './utils/apiTest';
-//import loginUserApi  from './utils/apiTest';
-=======
 import AlojamientosPage from './pages/alojamientos-user-page';
 import AlojamientosPublicPage from './pages/alojamientos-public-page';
->>>>>>> ceec898d46f0e1484140e13372fd7ae4c9564b47
+
 
 function App({ Component }) {
 
