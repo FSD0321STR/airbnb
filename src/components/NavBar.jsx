3bbfdbef7527d57cf8eb1ebb653305000f561da8
Saul Pane <saulpane@gmail.com>
2021-06-18 16:28:33 +0200

    anadida la search bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Flex, Box, Spacer, Menu,
   MenuButton,
   MenuList,
   MenuItem,
-  Button, Text } from "@chakra-ui/react"
-import { ChevronDownIcon } from '@chakra-ui/icons'
+  Button, InputGroup, InputLeftElement, Input } from "@chakra-ui/react"
+import { ChevronDownIcon, SearchIcon } from '@chakra-ui/icons'
 
 function NavBar() {
     
@@ -15,6 +15,15 @@ function NavBar() {
       <h2>Logo</h2>
       </Box>
       <Spacer />
+      <Box p={2}>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.500" />}
+        />
+        <Input type="text" placeholder="busca un alojamiento" />
+      </InputGroup>
+      </Box>
       <Box p={2}>
       <Link to="/"><Button mr={2} colorScheme="blue">Home</Button></Link>
         <Menu>
