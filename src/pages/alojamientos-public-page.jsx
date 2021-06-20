e141c42e8a09444f26b8d71d7463dda30751714a
Montserrat Cifuentes <cotamon88@gmail.com>
2021-06-20 17:36:35 +0200

    "mejoras pagina principal"

--- src/pages/alojamientos-public-page.jsx
+++ src/pages/alojamientos-public-page.jsx
@@ -1,72 +1,88 @@
 import React from "react";
-import { ChakraProvider, Grid, Box, Image, Flex, Badge, Text, Button } from "@chakra-ui/react";
+import { ChakraProvider, Grid, Box, Image, Flex, Badge, Text, Wrap,WrapItem, Center } from "@chakra-ui/react";
 import NavBarRegisterLogin from "../components/NavBar/navBarRegisterLogin";
 
 function AlojamientosPublicPage() {
     return (
         <ChakraProvider>
+            <Grid>
             <NavBarRegisterLogin />
-            <p>HOME</p>
-            <Grid templateColumns="repeat(3, 1fr)" gap={1}>
-                
-                <Box p="5" maxW="320px" borderWidth="1px">
-                <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
-                <Flex align="baseline" mt={2}>
-                    <Badge colorScheme="pink">Plus</Badge>
-                    <Text
-                    ml={2}
-                    textTransform="uppercase"
-                    fontSize="sm"
-                    fontWeight="bold"
-                    color="pink.800"
-                    >
-                    Verified &bull; Cape Town
-                    </Text>
-                </Flex>
-                <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-                    Modern, Chic Penthouse with Mountain, City & Sea Views
-                </Text>
-                <Text mb={1} mt={2}>€119/night</Text>
-                </Box>
-                <Box p="5" maxW="320px" borderWidth="1px">
-                <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
-                <Flex align="baseline" mt={2}>
-                    <Badge colorScheme="pink">Plus</Badge>
-                    <Text
-                    ml={2}
-                    textTransform="uppercase"
-                    fontSize="sm"
-                    fontWeight="bold"
-                    color="pink.800"
-                    >
-                    Verified &bull; Cape Town
-                    </Text>
-                </Flex>
-                <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-                    Modern, Chic Penthouse with Mountain, City & Sea Views
-                </Text>
-                <Text mb={1} mt={2}>€119/night</Text>
-                </Box>
-                <Box p="5" maxW="320px" borderWidth="1px">
-                <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
-                <Flex align="baseline" mt={2}>
-                    <Badge colorScheme="pink">Plus</Badge>
-                    <Text
-                    ml={2}
-                    textTransform="uppercase"
-                    fontSize="sm"
-                    fontWeight="bold"
-                    color="pink.800"
-                    >
-                    Verified &bull; Cape Town
-                    </Text>
-                </Flex>
-                <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-                    Modern, Chic Penthouse with Mountain, City & Sea Views
-                </Text>
-                <Text mb={1} mt={2}>€119/night</Text>
-                </Box>
             </Grid>
+            <p>HOME</p>
+            <Grid templateColumns="repeat(3, 1fr)" gap={4} marginTop="50%" marginLeft="3%">
+                <Wrap spacing="30px" >
+                    <WrapItem>
+                        
+                        <Box p="5" maxW="320px" borderWidth="1px">
+                        <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+                        <Flex align="baseline" mt={2}>
+                            <Badge colorScheme="pink">Plus</Badge>
+                            <Text
+                            ml={2}
+                            textTransform="uppercase"
+                            fontSize="sm"
+                            fontWeight="bold"
+                            color="pink.800"
+                            >
+                            Verified &bull; Cape Town
+                            </Text>
+                        </Flex>
+                        <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                            Modern, Chic Penthouse with Mountain, City & Sea Views
+                        </Text>
+                        <Text mb={1} mt={2}>€119/night</Text>
+                        </Box>
+                        
+                    </WrapItem>
+                    <WrapItem>
+                        <Center w="80%" h="40%" bg="red.200">
+                        <Box p="5" maxW="320px" borderWidth="1px">
+                        <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+                        <Flex align="baseline" mt={2}>
+                            <Badge colorScheme="pink">Plus</Badge>
+                            <Text
+                            ml={2}
+                            textTransform="uppercase"
+                            fontSize="sm"
+                            fontWeight="bold"
+                            color="pink.800"
+                            >
+                            Verified &bull; Cape Town
+                            </Text>
+                        </Flex>
+                        <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                            Modern, Chic Penthouse with Mountain, City & Sea Views
+                        </Text>
+                        <Text mb={1} mt={2}>€119/night</Text>
+                        </Box>
+                        </Center>
+                    </WrapItem>
+                    <WrapItem>
+                        <Center w="80%" h="40%" bg="red.200">
+                        <Box p="5" maxW="320px" borderWidth="1px">
+                        <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+                        <Flex align="baseline" mt={2}>
+                            <Badge colorScheme="pink">Plus</Badge>
+                            <Text
+                            ml={2}
+                            textTransform="uppercase"
+                            fontSize="sm"
+                            fontWeight="bold"
+                            color="pink.800"
+                            >
+                            Verified &bull; Cape Town
+                            </Text>
+                        </Flex>
+                        <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                            Modern, Chic Penthouse with Mountain, City & Sea Views
+                        </Text>
+                        <Text mb={1} mt={2}>€119/night</Text>
+                        </Box>
+                        </Center>
+                    </WrapItem>   
+                </Wrap>
+            </Grid>
+            
         </ChakraProvider>
       );
 }
