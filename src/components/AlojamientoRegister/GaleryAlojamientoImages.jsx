0fbfc2a1f01d5b6b3466021cee3fc2b0089f53a5
Montserrat Cifuentes <cotamon88@gmail.com>
2021-06-28 20:56:29 +0200

    "incorporando galeria de imagenes

--- src/components/AlojamientoRegister/GaleryAlojamientoImages.jsx
+++ src/components/AlojamientoRegister/GaleryAlojamientoImages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FormControl, FormLabel, Input, FormHelperText } from "@chakra-ui/react"
 import * as FilePond from 'filepond';
 import { parse } from 'filepond';
+import 'filepond/dist/filepond.min.css';
 
 const inputElement = document.querySelector('input[type="file"]');
 const pond = FilePond.create(inputElement);
@@ -12,7 +13,8 @@ function GaleryAlojamientoImages({value,onChange}) {
             <FormLabel fontSize="sm" m={1}>Guarda imagenes del alojamiento</FormLabel>
 
             <Input name="galery" value={value} onChange={onChange} type="file" placeholder="Galeria-imagenes" size="sm" colorScheme="blue"/>
-                <pond></pond>
+               
+                   
 
         </FormControl>
     )
