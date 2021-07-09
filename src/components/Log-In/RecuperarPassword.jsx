aad4f36e83bbe9ff6d0ba0a23d0c2baac89ef5f4
Montserrat Cifuentes <cotamon88@gmail.com>
2021-07-09 21:45:27 +0200

    " importando LINK de react-router-dom rn recuperar contraseña"

--- src/components/Log-In/RecuperarPassword.jsx
+++ src/components/Log-In/RecuperarPassword.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link, Text } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom";
 
 function RecuperarPassword () {
     return (
     <Text>{""}
-      <Link color="teal.500" href="/recuperar-contrasena">
+      <Link to="/recuperar-contrasena"  color="teal.500">
       ¿Has olvidado la contraseña?
       </Link>
     </Text>
