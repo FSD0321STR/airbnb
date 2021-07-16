7fb99c768672dc5468db8635ea029f429a371e6f
Carlos <dens.walker22@gmail.com>
2021-07-16 17:34:48 +0200

    date

--- src/components/Reservar-Habitación/Reserva-Form.jsx
+++ src/components/Reservar-Habitación/Reserva-Form.jsx
@@ -29,7 +29,6 @@ export default function ContactUs() {
     return(
         <Flex>
             <form onSubmit={sendEmail}>
-<<<<<<< HEAD
                     <Grid templateColumns="repeat(1, 1fr)" gap={10} marginTop="5rem" marginLeft= "41rem" marginBottom="15rem" border="ridge" bg="#FF385C" borderRadius="md">
                         <Center w="500px" textAlign='center'>
                             <Input type="text" className="form-control" border="solid" placeholder="Name" name="name" colorScheme="blue" size="sm" m={2}/>
@@ -52,24 +51,6 @@ export default function ContactUs() {
                         <Center w="500px" textAlign='center'>
                             <Input type="submit" className="btn btn-info" border="solid" background="white" value="Send Message"></Input>
                         </Center>
-=======
-                    <Grid templateColumns="repeat(1, 1fr)" gap={2} marginTop="1rem" border="ridge" bg="#FFffff" borderRadius="md">
-                        <Box p={2}>
-                            
-                            <Input type="text" className="form-control" border="solid" placeholder="Name" name="name" colorScheme="blue" size="sm" bg="Azure"/>
-                        
-                            <Input type="email" className="form-control" border="solid" placeholder="Email Address" name="email" size="sm" mt={2} bg="Azure"/>
-                        
-                            <Input type="text" className="form-control" border="solid" placeholder="Subject" name="subject" size="sm" mt={2} bg="Azure"/>
-                        
-                            <Input className="form-control" id="" cols="30" rows="8" border="solid" placeholder="¿Qué días viajarás?" name="date" size="sm" mt={2} bg="Azure"/>
-                        
-                            <Textarea className="form-control" id="" cols="30" rows="8" border="solid" placeholder="Your message" name="message" size="sm" mt={2} bg="Azure"></Textarea>
-                        
-                            <Input type="submit" className="btn btn-info" border="solid" background="white" value="Send Message" size="sm"  mt={2} bg="Azure"/>
-                            
-                        </Box>
->>>>>>> 00670237a5828fca5ca2c7a9efc9695a1edb08a5
                     </Grid>
             </form>
         </Flex>
